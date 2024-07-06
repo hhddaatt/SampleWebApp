236d0ab2b402996c9c5de10f462f89f3448eda07
hhddaatt <delonix55555@gmail.com>
2024-07-06 11:30:09 +0530

    Update TestCalculator.java

--- src/example/junit/TestCalculator.java
+++ src/example/junit/TestCalculator.java
@@ -24,8 +24,8 @@ public class TestCalculator {
 	 
 	 @Test
 	 public void testAdd() {
-		  double result = calc.add(10,10);
-		  org.junit.Assert.assertEquals(20, result, 0);
+		  double result = calc.add(10,20);
+		  org.junit.Assert.assertEquals(30, result, 0);
 	 }
 	 
 	 @Test
